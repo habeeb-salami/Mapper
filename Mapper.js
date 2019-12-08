0a30585962c72e54f4334b55340a11877f8e3973
Habeeb Salami <habeeb.salami@gmail.com>
2019-12-08 13:43:54 +0100

    upadated the Mapper object to have the shortest and longest distance properties

--- Mapper.js
+++ Mapper.js
@@ -1,26 +1,34 @@
 class Mapper {
+  constructor() {
+    this.shortestDist;
+    this.longestDist;
+  }
 
-    distance(obj1, obj2) {
-        let vx = obj1.x - obj2.x;
-        let vy = obj1.y - obj2.y;
-        const dist = Math.sqrt(vx * vx + vy * vy);
-        return dist;
+  distance(obj1, obj2) {
+    let vx = obj1.x - obj2.x;
+    let vy = obj1.y - obj2.y;
+    const dist = Math.sqrt(vx * vx + vy * vy);
+    return dist;
+  }
+  random() {
+    return Math.random() * 100; //between 1 and 100;
+  }
+  maxMin(distances) {
+    this.min = distances[1];
+    this.max = distances[1];
+    for (let i in distances) {
+      if (distances[i] >= this.max) {
+        //for catching the maximum number
+        this.max = distances[i];
+      }
+      if (distances[i] <= this.min) {
+        //for catching the maximum number
+        this.min = distances[i];
+      }
     }
-    random() {
-        return ((Math.random() * 100)); //between 1 and 100;
-    }
-    maxMin(distances) {
-        this.min = distances[1];
-        this.max = distances[1];
-        for (let i in distances) {
-            if (distances[i] >= this.max) { //for catching the maximum number
-                this.max = distances[i];
-            }
-            if (distances[i] <= this.min) { //for catching the maximum number
-                this.min = distances[i];
-            }
-        }
-        console.log(`Final Shortest Distance: ${this.min}, Final Longest Distance: ${this.max}`);
-    }
+    this.shortestDist = this.min;
+    this.longestDist = this.max;
+    console.log(`Final Shortest Distance: ${this.min}, Final Longest Distance: ${this.max}`);
+  }
 }
 module.exports = Mapper;
