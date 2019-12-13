c5ebc973251791293ba186c2c5c534d5ecfe2576
Habeeb Salami <habeeb.salami@gmail.com>
2019-12-13 03:09:38 +0100

    updated the test.js file

--- test/test.js
+++ test/test.js
@@ -3,23 +3,29 @@ const Entity = require("../Entity");
 var assert = require('chai').assert;
 
 
-describe('Map out shortest and longest distance', function() {
-  describe('#indexOf()', function() {
-	  let mapper = new Mapper(); //instantiate mapper
-	  let basePoint = new Entity(mapper.random(), mapper.random()); //for the base point
-      let otherPoints = {}; //for the list of other location of interest
-	  
-    it('should return the minimum and the maximum distance of the points provided', ()=> {
-		for (x = 1; x <= 100; ++x) {//populate the otherPoints with 100 Entities
-		  otherPoints[x] = new Entity(mapper.random(), mapper.random());
-		}
+describe('Map out shortest and longest distance', function () {
+	let mapper = new Mapper(); //instantiate mapper
+	let basePoint = new Entity(10.002, 14.85); //instantiate the basePoint/base Location
+	//instantiate the otherPoints/Locations
+	let otherPoints = [new Entity(200, 120),
+		new Entity(230, 170), new Entity(144.44, 122.9874),
+		new Entity(50, 20), new Entity(120, 144),
+		new Entity(200, 120)
+	];
+	it('It should test the Entity object Distance Property', () => {
+		assert.property(basePoint, 'distance');
+	});
+	// it('It should test the Mapper Object Distance Method', () => {
+	// 	assert.method(mapper, 'distance');
+	// });
+
+	it('should return the minimum and the maximum distance of the points provided', () => {
 		//Calculate Entities Distance
 		for (let key in otherPoints) {
-		  otherPoints[key].distance = mapper.distance(basePoint, otherPoints[key]);
+			otherPoints[key].distance = mapper.distance(basePoint, otherPoints[key]);
 		}
 		const minMax = mapper.maxMin(otherPoints); //get the maximum and the minimum distance of all the objects	
-		assert.property(minMax, 'min')
-	 
-    });
-  });
+		assert.property(minMax, 'min');
+
+	});
 });
