af576e73cd67ffa5e3d60882fbab7f8e685cb9d0
Habeeb Salami <habeeb.salami@gmail.com>
2019-12-13 03:55:12 +0100

    Mapper Branch updated with the master branch

--- Entity.js
+++ Entity.js
@@ -2,10 +2,7 @@ class Entity {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-<<<<<<< HEAD
-=======
         this.distance = null;
->>>>>>> c5ebc973251791293ba186c2c5c534d5ecfe2576
     }
 }
 module.exports = Entity;
--- Mapper.js
+++ Mapper.js
@@ -1,12 +1,4 @@
 class Mapper {
-<<<<<<< HEAD
-  // constructor() {
-  //   this.shortestDist;
-  //   this.longestDist;
-  // }
-=======
-
->>>>>>> c5ebc973251791293ba186c2c5c534d5ecfe2576
 
   distance(obj1, obj2) {
     let vx = obj1.x - obj2.x;
@@ -14,13 +6,6 @@ class Mapper {
     const dist = Math.sqrt(vx * vx + vy * vy);
     return dist;
   }
-<<<<<<< HEAD
-  random() {
-    return Math.random() * 100; //between 1 and 100;
-  }
-=======
-
->>>>>>> c5ebc973251791293ba186c2c5c534d5ecfe2576
   maxMin(otherPoints) {
     this.min = otherPoints[1].distance;
     this.max = otherPoints[1].distance;
@@ -32,19 +17,11 @@ class Mapper {
         this.min = otherPoints[i].distance; //assign to the min value the new found min distance
       }
     }
-<<<<<<< HEAD
-    console.log(`Final Shortest Distance: ${this.min}, Final Longest Distance: ${this.max}`);
-=======
     // console.log(`Final Shortest Distance: ${this.min}, Final Longest Distance: ${this.max}`);
->>>>>>> c5ebc973251791293ba186c2c5c534d5ecfe2576
     return {
       min: this.min,
       max: this.max
     };
   }
-<<<<<<< HEAD
-=======
-
->>>>>>> c5ebc973251791293ba186c2c5c534d5ecfe2576
 }
 module.exports = Mapper;
